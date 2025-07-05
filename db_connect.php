41ebe35558b9b2162ee0d1ec9059dd158332a0fc
sirly82 <srziafidah@gmail.com>
2025-07-05 20:29:45 +0700

    update database name

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@
 $servername = "localhost"; // Ganti dengan host database Anda
 $username = "root";      // Ganti dengan username database Anda
 $password = "";          // Ganti dengan password database Anda
-$dbname = "bakery";      // Nama database seperti yang didefinisikan di bakery.sql
+$dbname = "bakery_db";      // Nama database seperti yang didefinisikan di bakery.sql
 
 // Buat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
